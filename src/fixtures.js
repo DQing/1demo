e8d0607ce0754a832d199271d8e6e6e4f58076ad
douqing <15829258737@163.com>
2016-07-25 21:21:01 +0800

    添加promotion实例

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,27 +1,17 @@
 const allItems = require('./item');
+const promotion = require('./promotion');
 function loadAllItems() {
   return [
-    new allItems('ITEM000000','可口可乐','瓶',3.00),
-    new allItems('ITEM000001','雪碧','瓶',3.00),
-    new allItems('ITEM000002','苹果','斤',5.50),
-    new allItems('ITEM000003','荔枝','斤',15.00),
-    new allItems('ITEM000004','电池','个',2.00),
-    new allItems('ITEM000005','方便面','袋',4.50)
+    new allItems('ITEM000000', '可口可乐', '瓶', 3.00),
+    new allItems('ITEM000001', '雪碧', '瓶', 3.00),
+    new allItems('ITEM000002', '苹果', '斤', 5.50),
+    new allItems('ITEM000003', '荔枝', '斤', 15.00),
+    new allItems('ITEM000004', '电池', '个', 2.00),
+    new allItems('ITEM000005', '方便面', '袋', 4.50)
   ];
 }
 
 function loadPromotions() {
-  return [
-    {
-      type: 'BUY_TWO_GET_ONE_FREE',
-      barcodes: [
-        'ITEM000000',
-        'ITEM000001',
-        'ITEM000005'
-      ]
-    }
-  ];
+  return [new promotion('BUY_TWO_GET_ONE_FREE', ['ITEM000000', 'ITEM000001', 'ITEM000005'])];
 }
-
-exports.loadAllItems = loadAllItems;
-exports.loadPromotions = loadPromotions;
+module.exports = {loadPromotions:loadPromotions,loadAllItems:loadAllItems};
