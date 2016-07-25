525e373c2af38970abf373749856dd9cac03b4ec
douqing <15829258737@163.com>
2016-07-25 20:31:11 +0800

    创建Item类

--- src/item.js
+++ src/item.js
@@ -1,3 +1,9 @@
-/**
- * Created by douqing on 16-7-25.
- */
+class item{
+  constructor(barcode,name,unit,price = 0){
+    this.barcode = barcode;
+    this.name = name;
+    this.unit = unit;
+    this.price = price;
+  }
+}
+module.exports = item;
