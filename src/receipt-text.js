713c3b271eae276e0c4b1f4170de5bdb050bdfd8
douqing <15829258737@163.com>
2016-07-25 23:32:33 +0800

    创建ReceiptText类并且实现buildReceiptText方法

--- src/receipt-text.js
+++ src/receipt-text.js
@@ -1,3 +1,28 @@
-/**
- * Created by douqing on 16-7-25.
- */
+class ReceiptText {
+
+  static buildReceiptText(receipt) {
+    function formatMoney(money) {
+      return money.toFixed(2);
+    }
+
+
+    let receiptItemsText = receipt.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(receipt.total)}(元)
+节省：${formatMoney(receipt.savedTotal)}(元)
+**********************`;
+  }
+  
+}
+module.exports = ReceiptText;
